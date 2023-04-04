c79e1f1f351baa82adb1252b028a4922d647eb54
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2023-04-04 12:16:13 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-})
+    plugins: [react(), VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        includeAssets: ['favicon-96x96.png', 'android-icon-192x192.png'],
+        manifest: {
+            name: 'Dashboard',
+            short_name: 'Dashboard',
+            description: 'Admin Dashboard for Sales Transactions',
+            theme_color: '#000',
+            icons: [{
+                    src: 'android-chrome-192x192',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: 'android-chrome-512x512',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ],
+            // start_url: '/'
+        }
+    })],
+    define: {
+        global: "globalThis",
+        "process.env": {},
+    },
+});
